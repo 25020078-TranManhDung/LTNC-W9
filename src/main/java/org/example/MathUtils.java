bbaab65eb8508059d6bc30d0a167ba2205734e08
25020078-TranManhDung <25020078@vnu.edu.vn>
2026-05-04 20:08:19 +0700

    Bài 4: Tái cấu trúc getFilePath trong MathUtils để độc lập với OS sử dụng Path.of

--- src/main/java/org/example/MathUtils.java
+++ src/main/java/org/example/MathUtils.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.nio.file.Path;
+
 public class MathUtils {
   public static int max(int a, int b) {
     if (a >= b) {
@@ -16,7 +18,7 @@ public class MathUtils {
   }
 
   public static String getFilePath(String folder, String fileName) {
-    // Cố tình dùng dấu gạch chéo ngược của Windows
-    return folder + "\\" + fileName;
+    // Sử dụng Path.of để tự động chọn dấu gạch chéo đúng theo OS
+    return Path.of(folder, fileName).toString();
   }
 }
